16cf67479e09662ca7fb39765cfb49d0b50ba919
LucassNogueira <simplylucasjason@gmail.com>
2022-02-11 11:17:40 -0600

    getting an error on this third one when they all run together, going to move on to host then come back if i have time

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,15 +18,15 @@ test("Title shows up when page loads", async () => {
   expect(displayed).toBe(true);
 });
 
-// test("Draw button displays div with id choices", async () => {
-//   const drawButton = await driver.findElement(By.id("draw"));
-//   await drawButton.click();
-//   const divs = await driver.findElement(By.id("choices"));
-//   const displaying = await divs.isDisplayed();
-//   expect(displaying).toBe(true);
+test("Draw button displays div with id choices", async () => {
+  const drawButton = await driver.findElement(By.id("draw"));
+  await drawButton.click();
+  const divs = await driver.findElement(By.id("choices"));
+  const displaying = await divs.isDisplayed();
+  expect(displaying).toBe(true);
 
-//   await driver.sleep(5000);
-// });
+  await driver.sleep(5000);
+});
 
 test("adding a bot with add to duo button to your duo", async () => {
   const drawButton = await driver.findElement(By.id("draw"));
@@ -40,17 +40,17 @@ test("adding a bot with add to duo button to your duo", async () => {
   await driver.sleep(5000);
 });
 
-// test("removing bot", async () => {
-//   const drawButton = await driver.findElement(By.id("draw"));
-//   await drawButton.click();
-//   const divs = await driver.findElement(By.id("choices"));
-//   const botBtn = await driver.findElement(By.className("bot-btn"));
-//   await botBtn.click();
-//   const playerBot = await driver.findElement(By.id("player-duo"));
-//   await driver.sleep(2000);
-//   const removeBtn = await driver.findElement(By.linkText("Remove from Duo"));
-//   await removeBtn.click();
-//   expect(playerBot).toBe(false);
+test("removing bot", async () => {
+  const drawButton = await driver.findElement(By.id("draw"));
+  await drawButton.click();
+  const divs = await driver.findElement(By.id("choices"));
+  const botBtn = await driver.findElement(By.className("bot-btn"));
+  await botBtn.click();
+  const playerBot = await driver.findElement(By.id("player-duo"));
+  await driver.sleep(2000);
+  const removeBtn = await driver.findElement(By.linkText("Remove from Duo"));
+  await removeBtn.click();
+  expect(playerBot).toBe(false);
 
-//   await driver.sleep(5000);
-// });
+  await driver.sleep(5000);
+});
